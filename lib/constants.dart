1953370d680c2b3c4f47b61d37845f121df2bbc3
aakashchamolamac <aakashchamolababa@gmail.com>
2023-07-08 19:05:26 +0530

    added button widget

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgBlack = Color.fromARGB(255, 27, 26, 26);
+  static const Color bgButtonBlack1 = Color.fromARGB(255, 64, 63, 63);
+  static const Color bgButton2 = Color.fromARGB(255, 64, 63, 63);
+  static const Color buttonTextWhite1 = Color.fromARGB(255, 214, 214, 214);
+  static const Color buttonText2 = Color.fromARGB(255, 214, 214, 214);
+}
